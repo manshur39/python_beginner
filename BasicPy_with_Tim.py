3d42144cef1d43a1504360834cf561aff718dfa8
manshur39 <muhammadtm35@gmail.com>
2022-02-01 08:49:51 +0700

    basic operator dan input

---
+++ BasicPy_with_Tim.py
@@ -0,0 +1,23 @@
+#Basic operator and input :
+
+#print('what is your name?')
+#name = input()
+#print("Hallo", name)
+
+#aritmatika operator:
+num1= 20
+num2 = 5
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print (num1 / num2)
+print(num1 ** num2)
+print(num1 % num2)
+print(num1//num2)
+
+print("pick a number:")
+#num1= input()
+print("prick another number:")
+#num2= input()
+SUM = int(num1) + int(num2)
+print('Hasilnya:',SUM)
