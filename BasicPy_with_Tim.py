5f0cf503e3ab1824277d20c7c4a316a11346ecf4
manshur39 <muhammadtm35@gmail.com>
2022-02-01 09:04:51 +0700

    Video 3 : Conditions

--- BasicPy_with_Tim.py
+++ BasicPy_with_Tim.py
@@ -1,4 +1,4 @@
-#Basic operator and input :
+#Video 2 : Basic operator and input 
 
 #print('what is your name?')
 #name = input()
@@ -15,9 +15,23 @@ print(num1 ** num2)
 print(num1 % num2)
 print(num1//num2)
 
-print("pick a number:")
-#num1= input()
-print("prick another number:")
-#num2= input()
-SUM = int(num1) + int(num2)
-print('Hasilnya:',SUM)
+#print("pick a number:")
+# num1= input()
+# print("prick another number:")
+# num2= input()
+# SUM = int(num1) + int(num2)
+# print('Hasilnya:',SUM)
+
+#Video 3 : Conditions ( >,<,==,!=,>=,<=)
+a = 4
+b = 3
+print(a>b)
+print (a<b)
+print(a==b)
+print(a!=b)
+print(a<=b)
+print(a>=b)
+print('Hello' == 'Hello')
+print('hello' == 'HELLO')
+print(2+2 == 4)
+print (3+2 == 6)
